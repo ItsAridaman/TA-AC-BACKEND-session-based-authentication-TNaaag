04a15f453bcd8ad29f94170a26928fb029cbcbac
Aridaman singh <aridamansgh31@gmail.com>
2022-08-20 19:55:43 +0530

    Assignment-3 auth

--- block-BNaadx/AuthSample/Models/UserModel.js
+++ block-BNaadx/AuthSample/Models/UserModel.js
@@ -1,46 +1,45 @@
-var mongoose=require('mongoose');
-var Schema=mongoose.Schema;
-const bcrypt = require('bcrypt');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
 
 
-var UserSchema= new Schema(
+var UserSchema = new Schema(
     {
-        Name:{ type:String },
-        Username:{ type:String } ,
-        Password: { type:Number } 
+        Name: { type: String, unique:true },
+        Username: { type: String },
+        Password: { type: String },
+        IsAdmin:{type:Boolean, default:false},
+        Photo:{type:String}
     }
 );
 
-// UserSchema.pre('save', function(next)
-// {
-//     if(this.Password)
-//     {
+// UserSchema.pre('save', function (next) {
+//     if (this.Password && this.isModified('Password')) {
+//         console.log(this.Password);
 
-//         bcrypt.hash(this.Password, 10, function(err, hash) {
+//         bcrypt.hash(this.Password, 10, function (err, hashed) {
 //             // Store hash in your password DB.
-//             if(err) return next(err);
-//             this.Password=hash;
-//             return next()
+//             if (err) return next(err);
+//             this.Password = hashed;
+//             console.log(hashed);
+//             return next();
 //         });
-       
+
 //     }
-//     else
-//     {
+//     else {
 //         next()
 //     }
-   
+
 // });
 
-// UserSchema.methods.verifypassword=function(password,cb)
-// {
-//     bcrypt.compare(password,this.Password,(err,result)=>
-//     {
-//         return cb(err,result);
+// UserSchema.methods.verifypassword = function (password, cb) {
+//     bcrypt.compare(password, this.Password, (err, result) => {
+//         return cb(err, result);
 //     })
 // }
 
 
-   
-    
 
-module.exports=mongoose.model('user', UserSchema);
+
+
+module.exports = mongoose.model('user', UserSchema);
